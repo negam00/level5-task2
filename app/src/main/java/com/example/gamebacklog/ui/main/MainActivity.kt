fa5d5c0656a1fbafd610137a12296c8ad3a6a13c
negam00 <negam00@github.com>
2020-05-21 18:04:24 +0200

    implemented adapter + item touch helper

--- app/src/main/java/com/example/gamebacklog/ui/main/MainActivity.kt
+++ app/src/main/java/com/example/gamebacklog/ui/main/MainActivity.kt
@@ -1,25 +1,73 @@
 package com.example.gamebacklog.ui.main
 
 import android.os.Bundle
-import com.google.android.material.snackbar.Snackbar
-import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Observer
+import androidx.recyclerview.widget.ItemTouchHelper
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.gamebacklog.R
-
+import com.example.gamebacklog.model.Game
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.content_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private val viewModel: MainActivityViewModel by viewModels()
+    private val gameBacklog = arrayListOf<Game>()
+    private val backlogAdapter = BacklogAdapter(gameBacklog)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
 
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show()
+        initViews()
+        initViewModel()
+    }
+
+    private fun initViewModel() {
+        viewModel.games.observe(this, Observer { games ->
+            gameBacklog.clear()
+            gameBacklog.addAll(games)
+            backlogAdapter.notifyDataSetChanged()
+        })
+    }
+
+    fun initViews() {
+        rvGames.layoutManager = LinearLayoutManager(this, RecyclerView.VERTICAL, false)
+        rvGames.adapter = backlogAdapter
+
+        createItemTouchHelper().attachToRecyclerView(rvGames)
+
+        fab.setOnClickListener { openAddGame() }
+    }
+
+    private fun createItemTouchHelper(): ItemTouchHelper {
+        val callback = object : ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT) {
+            override fun onMove(
+                recyclerView: RecyclerView,
+                viewHolder: RecyclerView.ViewHolder,
+                target: RecyclerView.ViewHolder
+            ): Boolean {
+                return false
+            }
+
+            override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
+                val position = viewHolder.adapterPosition
+                val gameToDelete = gameBacklog[position]
+
+                viewModel.deleteGame(gameToDelete)
+            }
         }
+        return ItemTouchHelper(callback)
+    }
+
+    private fun openAddGame() {
+        TODO("Not yet implemented")
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
@@ -33,7 +81,7 @@ class MainActivity : AppCompatActivity() {
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         return when (item.itemId) {
-            R.id.action_settings -> true
+            R.id.action_delete_games -> true
             else -> super.onOptionsItemSelected(item)
         }
     }
